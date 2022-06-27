dd12d1a1f06a1643ed2e19f1089d7cbd3a73ad65
w17 <1050363049@qq.com>
2022-06-27 21:36:06 +0800

    wangyiqi

--- src/main.js
+++ src/main.js
@@ -1,2 +1,38 @@
 import "./tab";
 import "./banner";
+
+// jq的使用
+// src/mains.js
+// 先引入jquery
+import $ from 'jquery';
+$('#swiper').css('background-color', 'red')
+
+
+import "./styles/index.css"
+
+import "./styles/index.less"
+
+// index.js
+import imgUrl from './assets/1.gif';
+
+let img = document.createElement('img');
+img.src = imgUrl;
+document.body.appendChild(img);
+
+// 引入图片-使用
+import imgUrl1 from './assets/logo_small.png'
+const theImg1 = document.createElement("img")
+theImg1.src = imgUrl1
+document.body.appendChild(theImg1)
+
+// 引入字体图标文件
+import './assets/fonts/iconfont.css'
+
+class App {
+    static a = 123
+  }
+  
+console.log(App.a)
+const fn = () => {
+    console.log(11111);
+}
