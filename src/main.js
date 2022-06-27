080b96038caf6a090070f859883b9e8453a8a192
TCIano <740409310@qq.com>
2022-06-27 20:19:56 +0800

    ntcupdate

--- src/main.js
+++ src/main.js
@@ -1,2 +1,21 @@
 import "./tab";
 import "./banner";
+import $ from "jquery";
+// $("#swiper").css("background-color", "red");
+import "./style/index.css";
+
+// index.js
+import imgUrl from "./assets/1.gif";
+
+let img = document.createElement("img");
+img.src = imgUrl;
+document.body.appendChild(img);
+
+// 引入字体图标文件
+import "./assets/fonts/iconfont.css";
+
+class App {
+  static a = 123;
+}
+
+console.log(App.a);
