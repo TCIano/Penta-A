c530611fa7d19fd9116d58ef1e2438a7d9d3084f
Mynmhui <286734637@qq.com>
2022-06-27 21:35:17 +0800

    ahuidemo

--- src/main.js
+++ src/main.js
@@ -1,2 +1,29 @@
 import "./tab";
 import "./banner";
+// 引入jquery
+import $ from 'jquery';
+$('#swiper').css('background-color', 'aqua')
+
+// import './styles/index.css';
+import './styles/index.less';
+
+import imgUrl from './assets/1.gif';
+
+let img = document.createElement('img');
+img.src = imgUrl;
+document.body.appendChild(img);
+
+// import imgUrl1 from './assets/logo_small.png';
+
+// let img1 = document.createElement('img');
+// img1.src = imgUrl1;
+// document.body.appendChild(img1);
+
+// 引入字体图标文件
+import './assets/fonts/iconfont.css'
+
+class App {
+    static a = 123
+  }
+  
+  console.log(App.a)
